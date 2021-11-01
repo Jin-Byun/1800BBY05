4b1917771ae8377cd4cc379a89cefbd3da5c8fab
Jin Byun <jbyun10@my.bcit.ca>
2021-11-01 08:59:10 -0700

    Added firebase auth to the html and firebaseapi.js with new project.

---
+++ scripts/firebaseAPI.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+let firebaseConfig = {
+  apiKey: "AIzaSyB4BIwUmrFoAhe8rPZZnSqTBsRjdsh-7vM",
+    authDomain: "proj-bby05.firebaseapp.com",
+    projectId: "proj-bby05",
+    storageBucket: "proj-bby05.appspot.com",
+    messagingSenderId: "226932291387",
+    appId: "1:226932291387:web:a8e53220c4229c4605e91e"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
